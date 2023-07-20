717fd658c1c7079c3ee2755fe6eee0e0c0efd0e3
varun-s20 <varun.jcfanx@gmail.com>
2023-07-20 21:38:29 +0530

    background color selector with lighter tints and darker shades spectrum

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Values from 'values.js'
+import SingleColor from './SingleColor'
 
 function App() {
+
+  const [color, setColor] = useState("")
+  const [error, setError] = useState(false)
+  const [list, setList] = useState(new Values("#f15025").all(10))
+
+  const handleSubmit = (e) => {  
+    e.preventDefault();
+
+    try{
+      let colors = new Values(color).all(10);
+      console.log(colors)
+      setList(colors)
+    }catch(error){
+      setError(error)
+      console.log(error)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <section className='container'>
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder="#f15025"
+            className={`${error ? 'error': null}`}
+          />
+          <button className='btn' type='submit'>Submit</button>
+        </form>
+      </section>
+
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor 
+              key ={index}
+              {...color}
+              index = {index}            
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
